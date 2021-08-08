32edb85f0e010d10b081441fcce0b6838c7dad2e
qq1065507891 <42400109+qq1065507891@users.noreply.github.com>
2021-08-08 15:48:16 +0800

    Update training.py

--- src/training.py
+++ src/training.py
@@ -5,13 +5,13 @@ import time
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 from src.utils.process import process
-from src.utils.utils import load_pkl, training_curve, get_time_idf
+from src.utils.utils import load_pkl, training_curve, get_time_idf, load_embedding, get_embedding_matrix
 from src.utils.dataset import Dateset
 from src.models.TextCNN import TextCNN
 
 
-def init_model(config, model_name, vocab_size):
-    model = TextCNN(config, model_name, vocab_size).build_model()
+def init_model(config, model_name, vocab_size, embedding_matrix=None):
+    model = TextCNN(config, model_name, vocab_size, embedding_matrix).build_model()
     model.compile(
         loss=tf.keras.losses.categorical_crossentropy,
         optimizer=tf.keras.optimizers.Adam(config.learning_rate),
@@ -20,8 +20,8 @@ def init_model(config, model_name, vocab_size):
     return model
 
 
-def load_model(config, model_name, vocab_size):
-    model = init_model(config, model_name, vocab_size)
+def load_model(config, model_name, vocab_size, embedding_matrix=None):
+    model = init_model(config, model_name, vocab_size, embedding_matrix=embedding_matrix)
     model.load_weights(config.model_pkl)
     return model
 
@@ -33,11 +33,14 @@ def train(config, model_name):
     train_data_path = os.path.join(config.out_path, 'train.pkl')
     test_data_path = os.path.join(config.out_path, 'test.pkl')
     vocab_path = os.path.join(config.out_path, 'vocab.pkl')
+    embedding_path = os.path.join(config.data_path, 'vec.txt')
 
+    embedding = load_embedding(embedding_path)
     train_data = load_pkl(train_data_path, 'train_data')
     test_data = load_pkl(test_data_path, 'test_data')
     vocab = load_pkl(vocab_path, 'vocab')
 
+    embedding_matrix = get_embedding_matrix(config, embedding, vocab)
     vocab_size = len(vocab.word2id)
     start_time = time.time()
 
@@ -54,12 +57,12 @@ def train(config, model_name):
 
     print('加载模型')
     if os.path.exists(config.model_pkl):
-        model = load_model(config, model_name, vocab_size)
+        model = load_model(config, model_name, vocab_size, embedding_matrix)
     else:
-        model = init_model(config, model_name, vocab_size)
+        model = init_model(config, model_name, vocab_size, embedding_matrix)
 
     callbacks = [
-        # EarlyStopping(monitor='val_accuracy', patience=5, verbose=1, mode='max'),
+        EarlyStopping(monitor='val_accuracy', patience=5, verbose=1, mode='max'),
         ModelCheckpoint(config.model_pkl, monitor='val_accuracy', verbose=1,
                         save_weights_only=True, save_best_only=True, mode='max'),
     ]
@@ -76,4 +79,4 @@ def train(config, model_name):
     end_time = get_time_idf(start_time)
     print(f'训练完成, 用时: {end_time}')
     training_curve(history.history['loss'], history.history['accuracy'],
-                   history.history['val_loss'], history.history['accuracy'])
+                   history.history['val_loss'], history.history['val_accuracy'])
